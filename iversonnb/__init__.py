9ea4f3da97c1c399542e524284a1abe16ec7455c
Jeremy Howard <github@jhoward.fastmail.fm>
2026-08-03 20:54:12 +1000

    fixes #9

--- iversonnb/__init__.py
+++
@@ -1,9 +0,0 @@
-__version__ = "0.2.1"
-
-from .core import *
-from .j import create_j_magic
-
-def load_ipython_extension(ipython):
-    "Register both the `apl` and `j` magics"
-    create_magic(shell=ipython)
-    create_j_magic(shell=ipython)
